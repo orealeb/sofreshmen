c51a674278b7ce2595e8488b791186610718fa57
unknown <alebios2@SIEBL-4303-06.ad.uillinois.edu>
2015-05-09 07:05:13 -0500

    gg'

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.set('port', process.env.PORT || '5000');
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/www"));
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
